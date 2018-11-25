a079aa2d14e916c3c70f8f75a27f81a094b67575
Xon <tazipper1@hotmail.com>
2018-11-25 18:58:22 +0800

    Set reporter user when doing logOperation

--- upload/src/addons/SV/ReportImprovements/XF/Repository/Warning.php
+++ upload/src/addons/SV/ReportImprovements/XF/Repository/Warning.php
@@ -2,9 +2,10 @@
 
 namespace SV\ReportImprovements\XF\Repository;
 
+use SV\ReportImprovements\Globals;
+
 /**
  * Class Warning
- * 
  * Extends \XF\Repository\Warning
  *
  * @package SV\ReportImprovements\XF\Repository
@@ -12,16 +13,34 @@ namespace SV\ReportImprovements\XF\Repository;
 class Warning extends XFCP_Warning
 {
     /**
+     * @noinspection PhpDocMissingThrowsInspection
      * @param \XF\Entity\Warning $warning
-     * @param string $type
+     * @param string             $type
      */
     public function logOperation(\XF\Entity\Warning $warning, $type)
     {
-        /** @var \SV\ReportImprovements\Service\WarningLog\Creator $warningLogCreator */
-        $warningLogCreator = $this->app()->service('SV\ReportImprovements:WarningLog\Creator', $warning, $type);
-        if ($warningLogCreator->validate($errors))
+        $reporter = \XF::visitor();
+        $options = \XF::options();
+        if (Globals::$expiringFromCron || !$reporter->user_id)
         {
-            $warningLogCreator->save();
+            $reporter = $this->app()->find('XF:User', $options->sv_ri_user_id ?: 1);
+            if (!$reporter)
+            {
+                $reporter = $this->app()->find('XF:User', 1);
+            }
+            if (!$reporter)
+            {
+                $reporter = \XF::visitor();
+            }
         }
+
+        \XF::asVisitor($reporter, function () use ($warning, $type) {
+            /** @var \SV\ReportImprovements\Service\WarningLog\Creator $warningLogCreator */
+            $warningLogCreator = $this->app()->service('SV\ReportImprovements:WarningLog\Creator', $warning, $type);
+            if ($warningLogCreator->validate($errors))
+            {
+                $warningLogCreator->save();
+            }
+        });
     }
 }
