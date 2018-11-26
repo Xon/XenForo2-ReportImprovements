ced6fee4c351f0519ae11e545cf724fa0d552e01
TickTackk <31729605+ticktackk@users.noreply.github.com>
2018-11-26 13:31:33 +0530

    Allow report to be resolved when viewing warning

--- upload/src/addons/SV/ReportImprovements/XF/Pub/Controller/Warning.php
+++ upload/src/addons/SV/ReportImprovements/XF/Pub/Controller/Warning.php
@@ -2,6 +2,8 @@
 
 namespace SV\ReportImprovements\XF\Pub\Controller;
 
+use XF\Mvc\ParameterBag;
+
 /**
  * Class Warning
  * 
@@ -22,5 +24,49 @@ class Warning extends XFCP_Warning
     {
         $extraWith[] = 'Report';
         return parent::assertViewableWarning($id, $extraWith);
+    }
+
+    /**
+     * @param ParameterBag $parameterBag
+     *
+     * @return \XF\Mvc\Reply\Error|\XF\Mvc\Reply\Redirect|\XF\Mvc\Reply\Reroute
+     * @throws \XF\Mvc\Reply\Exception
+     */
+    public function actionResolveReport(ParameterBag $parameterBag)
+    {
+        $this->assertPostOnly();
+
+        /** @var \SV\ReportImprovements\XF\Entity\Warning $warning */
+        $warning = $this->assertViewableWarning($parameterBag->warning_id);
+        if (!$report = $warning->Report)
+        {
+            throw $this->exception($this->notFound(\XF::phrase('requested_report_not_found')));
+        }
+
+        if (!$report->canUpdate($error) || $report->isClosed())
+        {
+            return $this->noPermission($error);
+        }
+
+        if ($this->filter('resolve', 'bool'))
+        {
+            /** @var \XF\Service\Report\Commenter $commenter */
+            $commenter = $this->service('XF:Report\Commenter', $report);
+            $commenter->setReportState('resolved');
+            if (!$commenter->validate($errors))
+            {
+                return $this->error($errors);
+            }
+            $commenter->sendNotifications();
+
+            $report = $commenter->getReport();
+            $report->draft_comment->delete();
+
+            $this->session()->reportLastRead = \XF::$time;
+
+            return $this->redirect($this->getDynamicRedirect());
+        }
+
+        return $this->rerouteController('XF:Warning', 'index', $parameterBag->params());
     }
 }
