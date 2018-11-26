2b987453c2bc5721b17d495929ca261bcdcff9cf
Xon <tazipper1@hotmail.com>
2018-11-26 16:23:55 +0800

    If criteriaUser returns false, the code event listener stops iterating checking for that item, return not-false to keep searching

--- upload/src/addons/SV/ReportImprovements/Listener.php
+++ upload/src/addons/SV/ReportImprovements/Listener.php
@@ -10,12 +10,13 @@ namespace SV\ReportImprovements;
 class Listener
 {
     /**
-     * @param string $rule
+     * @param string          $rule
      * @param array           $data
      * @param \XF\Entity\User $user
-     * @param                 $eventReturnValue
+     * @param bool            $eventReturnValue
+     * @return bool
      */
-    public static function criteriaUser($rule, array $data, \XF\Entity\User $user, &$eventReturnValue) : void
+    public static function criteriaUser($rule, array $data, \XF\Entity\User $user, &$eventReturnValue)
     {
         /** @var \SV\ReportImprovements\XF\Repository\Report $reportRepo */
         $reportRepo = \XF::repository('XF:Report');
@@ -27,70 +28,73 @@ class Listener
                 {
                     $eventReturnValue = true;
                 }
-                break;
+                return false;
 
             case 'sv_reports_maximum':
                 if ($reportRepo->countReportsByUser($user, (int) $data['days']) <= $data['reports'])
                 {
                     $eventReturnValue = true;
                 }
-                break;
+                return false;
 
             case 'sv_reports_minimum_open':
                 if ($reportRepo->countReportsByUser($user, (int) $data['days'], 'open') >= $data['reports'])
                 {
                     $eventReturnValue = true;
                 }
-                break;
+                return false;
 
             case 'sv_reports_maximum_open':
                 if ($reportRepo->countReportsByUser($user, (int) $data['days'], 'open') <= $data['reports'])
                 {
                     $eventReturnValue = true;
                 }
-                break;
+                return false;
 
             case 'sv_reports_minimum_assigned':
                 if ($reportRepo->countReportsByUser($user, (int) $data['days'], 'assigned') >= $data['reports'])
                 {
                     $eventReturnValue = true;
                 }
-                break;
+                return false;
 
             case 'sv_reports_maximum_assigned':
                 if ($reportRepo->countReportsByUser($user, (int) $data['days'], 'assigned') <= $data['reports'])
                 {
                     $eventReturnValue = true;
                 }
-                break;
+                return false;
 
             case 'sv_reports_minimum_resolved':
                 if ($reportRepo->countReportsByUser($user, (int) $data['days'], 'resolved') >= $data['reports'])
                 {
                     $eventReturnValue = true;
                 }
-                break;
+                return false;
 
             case 'sv_reports_maximum_resolved':
                 if ($reportRepo->countReportsByUser($user, (int) $data['days'], 'resolved') <= $data['reports'])
                 {
                     $eventReturnValue = true;
                 }
-                break;
+                return false;
 
             case 'sv_reports_minimum_rejected':
                 if ($reportRepo->countReportsByUser($user, (int) $data['days'], 'rejected') >= $data['reports'])
                 {
                     $eventReturnValue = true;
                 }
-                break;
+                return false;
 
             case 'sv_reports_maximum_rejected':
                 if ($reportRepo->countReportsByUser($user, (int) $data['days'], 'rejected') <= $data['reports'])
                 {
                     $eventReturnValue = true;
                 }
-                break;
+                return false;
+
+            default:
+                return true;
         }
     }
 }
