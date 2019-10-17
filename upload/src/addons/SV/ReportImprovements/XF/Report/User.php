d9ab058d26582bf27d9de32b5bf8db3ad035ce44
Xon <tazipper1@hotmail.com>
2019-10-18 05:23:18 +0800

    Remove XF2.0 support

--- upload/src/addons/SV/ReportImprovements/XF/Report/User.php
+++ upload/src/addons/SV/ReportImprovements/XF/Report/User.php
@@ -24,38 +24,6 @@ class User extends XFCP_User
         return $visitor->canViewUserReport();
     }
 
-    /**
-     * @param Report $report
-     * @return \XF\Phrase
-     */
-    public function getContentTitle(Report $report)
-    {
-        if ((\XF::$versionId > 2001170 && \XF::$versionId < 2010000) || \XF::$versionId > 2010032)
-        {
-            return parent::getContentTitle($report);
-        }
-
-        // patch an XF2.0.12 bug
-        $content = $report->content_info;
-
-        if (isset($content['username']))
-        {
-            $name = $content['username'];
-        }
-        else if (isset($content['user']['username']))
-        {
-            $name = $content['user']['username'];
-        }
-        else
-        {
-            $name = \XF::phrase('guest');
-        }
-
-        return \XF::phrase('member_x', [
-            'username' => $name,
-        ]);
-    }
-
     public function getContentLink(Report $report)
     {
         $reportInfo = $report->content_info;
