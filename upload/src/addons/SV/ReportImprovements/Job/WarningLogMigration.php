ecce013d512ec3bec80e40fde905b19743119cf2
Xon <tazipper1@hotmail.com>
2021-12-11 03:54:19 +0800

    Add work-around for linking warnings to reports for partially deleted content

--- upload/src/addons/SV/ReportImprovements/Job/WarningLogMigration.php
+++ upload/src/addons/SV/ReportImprovements/Job/WarningLogMigration.php
@@ -40,8 +40,14 @@ class WarningLogMigration extends AbstractRebuildJob
     {
         /** @var \SV\ReportImprovements\XF\Entity\Warning $warning */
         $warning = $this->app->em()->find('XF:Warning', $id, ['User', 'WarnedBy']);
-        if ($warning)
+        if ($warning !== null)
         {
+            // avoid known cases where a post is detached from a thread, as this causes an error in setupReportEntityContent
+            $content = $warning->Content ?? null;
+            if ($content instanceof \XF\Entity\Post && $content->Thread === null)
+            {
+                return;
+            }
             Globals::$expiringFromCron = false;
             $user = $warning->WarnedBy;
             if (!$user)
@@ -72,6 +78,18 @@ class WarningLogMigration extends AbstractRebuildJob
                     {
                         $warningLogCreator->save();
                     }
+                }
+                catch (\Exception $e)
+                {
+                    // setupReportEntityContent can throw if the child content is detached from the parent
+                    // but there is really no sane way to detect this ahead of time
+                    if (\stripos($e->getMessage(), 'Attempt to read property') !== false)
+                    {
+                        \XF::logException($e);
+
+                        return;
+                    }
+                    throw $e;
                 }
                 finally
                 {
