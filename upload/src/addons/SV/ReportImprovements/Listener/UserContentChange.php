0c857399d1ab26156026d97a2fe7cdaed2bdcf15
Xon <635541+Xon@users.noreply.github.com>
2023-04-05 09:39:01 +0800

    Fix support for user renames, deletes/merges

---
+++ upload/src/addons/SV/ReportImprovements/Listener/UserContentChange.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SV\ReportImprovements\Listener;
+
+use XF\Service\User\ContentChange;
+
+abstract class UserContentChange
+{
+    private function __construct() { }
+
+    /** @noinspection PhpUnusedParameterInspection */
+    public static function userContentChangeInit(ContentChange $changeService, array &$updates): void
+    {
+        $updates['xf_report'][] = ['assigner_user_id'];
+
+        if (is_array($updates['xf_report_comment']) && is_array($updates['xf_report_comment'][0]))
+        {
+            $updates['xf_report_comment'] = [$updates['xf_report_comment']];
+        }
+        $updates['xf_report_comment'][] = ['assigned_user_id', 'assigned_username'];
+        $updates['xf_report_comment'][] = ['last_edit_user_id'];
+
+        if (!isset($updates['xf_sv_warning_log']))
+        {
+            $updates['xf_sv_warning_log'] = [];
+        }
+
+        $updates['xf_sv_warning_log'][] = ['user_id'];
+        $updates['xf_sv_warning_log'][] = ['warning_user_id'];
+    }
+}
