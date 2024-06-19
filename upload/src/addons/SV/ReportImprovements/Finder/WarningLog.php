2fd1e546673bf88dc12b43cc32c199fcd8efc9df
Xon <635541+Xon@users.noreply.github.com>
2024-06-20 02:55:02 +0800

    Autogenerate finders

--- upload/src/addons/SV/ReportImprovements/Finder/WarningLog.php
+++ upload/src/addons/SV/ReportImprovements/Finder/WarningLog.php
@@ -3,7 +3,15 @@
 namespace SV\ReportImprovements\Finder;
 
 use XF\Mvc\Entity\Finder;
+use XF\Mvc\Entity\AbstractCollection as AbstractCollection;
+use SV\ReportImprovements\Entity\WarningLog as WarningLogEntity;
 
+/**
+ * @method AbstractCollection<WarningLogEntity>|WarningLogEntity[] fetch(?int $limit = null, ?int $offset = null)
+ * @method WarningLogEntity|null fetchOne(?int $offset = null)
+ * @implements \IteratorAggregate<string|int,WarningLogEntity>
+ * @extends Finder<WarningLogEntity>
+ */
 class WarningLog extends Finder
 {
 
