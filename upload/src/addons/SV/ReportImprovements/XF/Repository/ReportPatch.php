59fe41883a5188b8b224cfd8aeed20514800ce5d
Xon <635541+Xon@users.noreply.github.com>
2024-07-10 04:22:21 +0800

    Fix class alias

--- upload/src/addons/SV/ReportImprovements/XF/Repository/ReportPatch.php
+++ upload/src/addons/SV/ReportImprovements/XF/Repository/ReportPatch.php
@@ -8,8 +8,8 @@ namespace SV\ReportImprovements\XF\Repository;
 
 /** @noinspection PhpUndefinedClassInspection */
 \SV\StandardLib\Helper::repo()->aliasClass(
-    SearchPatch::class,
+    ReportPatch::class,
     \XF::$versionId < 2030000
-        ? \SV\ReportImprovements\XF\Repository\XF22\SearchPatch::class
-        : \SV\ReportImprovements\XF\Repository\XF23\SearchPatch::class
+        ? \SV\ReportImprovements\XF\Repository\XF22\ReportPatch::class
+        : \SV\ReportImprovements\XF\Repository\XF23\ReportPatch::class
 );
